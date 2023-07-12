ded777606e132d7643d46fa48c0ed85d547d7afc
이은우 <80537533+lew0205@users.noreply.github.com>
2023-07-12 19:48:33 +0900

    Initial commit

---
+++ src/main/kotlin/com/devholu/awstuto/AwstutoApplication.kt
@@ -0,0 +1,11 @@
+package com.devholu.awstuto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwstutoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AwstutoApplication>(*args)
+}
---
+++ src/test/kotlin/com/devholu/awstuto/AwstutoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.devholu.awstuto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AwstutoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
